f09b03e1a5eb8cf3c9acff74ae50b27d6fff5fbc
Cristian Pop <1867383+Cristi075@users.noreply.github.com>
2020-08-06 08:58:50 +0300

    Add demo for extracting data (only current day)

--- covid_news.py
+++ covid_news.py
@@ -8,12 +8,29 @@ data_url = 'https://di5ds1eotmbx1.cloudfront.net/latestData.json'
 
 # The counties that I want to display besides the nation-wide numbers
 # TODO: Implement this. After the numbers for nation-wide are displayed nicely
-watched_counties = ['BN', 'CJ']
+watched_counties = ['BN', 'CJ', 'MM']
 
 
-# Extracts relevant data (for me) and returns a dictionary
-def extract_data(json_data):
-    None
+# Extracts relevant data (for me) and returns a list of dictionaries
+def extract_current_day_data(json_data):
+    result = []
+
+    # First, extract nation-wise numbers
+    current_day_data = json_data['currentDayStats']
+    if not current_day_data['complete']:
+        print('Warning: Complete flag for current day is set to false')
+
+    tmp_result = {'days_ago': 0, 'name': 'Romania', 'infected': current_day_data['numberInfected']}
+    result.append(tmp_result)
+
+    # Then, extract data for watched counties
+    for county in current_day_data['countyInfectionsNumbers']:
+        if county in watched_counties:
+            tmp_result1 = {'days_ago': 0, 'name': county,
+                           'infected': current_day_data['countyInfectionsNumbers'][county]}
+            result.append(tmp_result1)
+
+    return result
 
 
 def fetch_data():
@@ -43,6 +60,10 @@ def main():
     # Display when the data is from
     time = datetime.fromtimestamp(data['lasUpdatedOn'])
     print('Using data from %s' % time)
+
+    # Obtain the data that I'm interested in
+    current_day_data = extract_current_day_data(data)
+    print(current_day_data)
 
 
 if __name__ == '__main__':
